aff2f2cf1ed350709de1751a53703e860a060370
Francesco Evangelista <francesco.evangelista@gmail.com>
2015-05-04 14:28:00 -0400

    Fixed 1-RDM code.

--- wavefunction_rdm.cc
+++ wavefunction_rdm.cc
@@ -7,6 +7,8 @@
 // *
 // */
 
+#include <cmath>
+
 #include <boost/timer.hpp>
 
 #include <libqt/qt.h>
@@ -14,6 +16,17 @@
 #include "wavefunction.h"
 
 namespace psi{ namespace libadaptive{
+
+/**
+ * Compute the one-particle density matrix for a given wave function
+ * @param alfa flag for alfa or beta component, true = alfa, false = beta
+ */
+void FCIWfn::compute_rdms(int max_order)
+{
+    compute_1rdm(opdm_a_,true);
+    compute_1rdm(opdm_b_,false);
+    compute_2rdm_aa(tpdm_aa_,true);
+}
 
 /**
  * Compute the one-particle density matrix for a given wave function
@@ -58,7 +71,7 @@ void FCIWfn::compute_1rdm(std::vector<double>& rdm, bool alfa)
                             double* y = &(Ch[vo[ss].J][0]);
                             double* c = &(Ch[vo[ss].I][0]);
                             for(size_t L = 0; L < maxL; ++L){
-                                rdm[oei_index(p_abs,q_abs)] += c[L] * y[L] * H;
+                                rdm[p_abs * ncmo_ + q_abs] += c[L] * y[L] * H;
                             }
                         }
                     }
@@ -70,9 +83,138 @@ void FCIWfn::compute_1rdm(std::vector<double>& rdm, bool alfa)
     for (int p = 0; p < ncmo_; ++p) {
         outfile->Printf("\n");
         for (int q = 0; q < ncmo_; ++q) {
-            outfile->Printf("%9.6f ",rdm[oei_index(p,q)]);
+            outfile->Printf("%15.12f ",rdm[oei_index(p,q)]);
         }
     }
+}
+
+
+/**
+ * Compute the aa/bb two-particle density matrix for a given wave function
+ * @param alfa flag for alfa or beta component, true = aa, false = bb
+ */
+void FCIWfn::compute_2rdm_aa(std::vector<double>& rdm, bool alfa)
+{
+    rdm.assign(ncmo_ * ncmo_ * ncmo_ * ncmo_,0.0);
+    // Notation
+    // ha - symmetry of alpha strings
+    // hb - symmetry of beta strings
+    for(int ha = 0; ha < nirrep_; ++ha){
+        int hb = ha ^ symmetry_;
+        if(detpi_[ha] > 0){
+            SharedMatrix C = alfa ? C_[ha] : C1;
+            double** Ch = C->pointer();
+
+            if(!alfa){
+                C->zero();
+                size_t maxIa = alfa_graph_->strpi(ha);
+                size_t maxIb = beta_graph_->strpi(hb);
+
+                double** C0h = C_[ha]->pointer();
+
+                // Copy C0 transposed in C1
+                for(size_t Ia = 0; Ia < maxIa; ++Ia)
+                    for(size_t Ib = 0; Ib < maxIb; ++Ib)
+                        Ch[Ib][Ia] = C0h[Ia][Ib];
+            }
+
+            size_t maxL = alfa ? beta_graph_->strpi(hb) : alfa_graph_->strpi(ha);
+            // Loop over (p>q) == (p>q)
+            for(int pq_sym = 0; pq_sym < nirrep_; ++pq_sym){
+                size_t max_pq = lists_->pairpi(pq_sym);
+                for(size_t pq = 0; pq < max_pq; ++pq){
+                    const Pair& pq_pair = lists_->get_nn_list_pair(pq_sym,pq);
+                    int p_abs = pq_pair.first;
+                    int q_abs = pq_pair.second;
+
+                    double integral = alfa ? tei_aaaa(p_abs,q_abs,p_abs,q_abs) : tei_bbbb(p_abs,q_abs,p_abs,q_abs);
+
+                    std::vector<StringSubstitution>& OO = alfa ? lists_->get_alfa_oo_list(pq_sym,pq,ha)
+                                                               : lists_->get_beta_oo_list(pq_sym,pq,hb);
+
+                    size_t maxss = OO.size();
+                    for(size_t ss = 0; ss < maxss; ++ss){
+//                        C_DAXPY(maxL,static_cast<double>(OO[ss].sign) * integral, &(C->pointer()[OO[ss].I][0]), 1, &(Y->pointer()[OO[ss].J][0]), 1);
+                        double H = static_cast<double>(OO[ss].sign);
+                        double* y = &(Ch[OO[ss].J][0]);
+                        double* c = &(Ch[OO[ss].I][0]);
+                        for(size_t L = 0; L < maxL; ++L){
+                            rdm[tei_index(p_abs,q_abs,p_abs,q_abs)] += c[L] * y[L] * H;
+                        }
+                    }
+                }
+            }
+            // Loop over (p>q) > (r>s)
+            for(int pq_sym = 0; pq_sym < nirrep_; ++pq_sym){
+                size_t max_pq = lists_->pairpi(pq_sym);
+                for(size_t pq = 0; pq < max_pq; ++pq){
+                    const Pair& pq_pair = lists_->get_nn_list_pair(pq_sym,pq);
+                    int p_abs = pq_pair.first;
+                    int q_abs = pq_pair.second;
+                    for(size_t rs = 0; rs < pq; ++rs){
+                        const Pair& rs_pair = lists_->get_nn_list_pair(pq_sym,rs);
+                        int r_abs = rs_pair.first;
+                        int s_abs = rs_pair.second;
+                        double integral = alfa ? tei_aaaa(p_abs,q_abs,r_abs,s_abs) : tei_bbbb(p_abs,q_abs,r_abs,s_abs);
+
+                        {
+                            std::vector<StringSubstitution>& VVOO = alfa ? lists_->get_alfa_vvoo_list(p_abs,q_abs,r_abs,s_abs,ha)
+                                                                         : lists_->get_beta_vvoo_list(p_abs,q_abs,r_abs,s_abs,hb);
+                            // TODO loop in a differen way
+                            size_t maxss = VVOO.size();
+                            for(size_t ss = 0; ss < maxss; ++ss){
+                                double H = static_cast<double>(VVOO[ss].sign);
+                                double* y = &(Ch[VVOO[ss].J][0]);
+                                double* c = &(Ch[VVOO[ss].I][0]);
+                                for(size_t L = 0; L < maxL; ++L){
+                                    rdm[tei_index(p_abs,q_abs,r_abs,s_abs)] += c[L] * y[L] * H;
+                                }
+                            }
+//                                C_DAXPY(maxL,static_cast<double>(VVOO[ss].sign) * integral, &(C->pointer()[VVOO[ss].I][0]), 1, &(Y->pointer()[VVOO[ss].J][0]), 1);
+                        }
+                        {
+                            std::vector<StringSubstitution>& VVOO = alfa ? lists_->get_alfa_vvoo_list(r_abs,s_abs,p_abs,q_abs,ha)
+                                                                         : lists_->get_beta_vvoo_list(r_abs,s_abs,p_abs,q_abs,hb);
+                            // TODO loop in a differen way
+                            size_t maxss = VVOO.size();
+                            for(size_t ss = 0; ss < maxss; ++ss){
+                                double H = static_cast<double>(VVOO[ss].sign);
+                                double* y = &(Ch[VVOO[ss].J][0]);
+                                double* c = &(Ch[VVOO[ss].I][0]);
+                                for(size_t L = 0; L < maxL; ++L){
+                                    rdm[tei_index(p_abs,q_abs,r_abs,s_abs)] += c[L] * y[L] * H;
+                                }
+                            }
+//                                C_DAXPY(maxL,static_cast<double>(VVOO[ss].sign) * integral, &(C->pointer()[VVOO[ss].I][0]), 1, &(Y->pointer()[VVOO[ss].J][0]), 1);
+                        }
+                    }
+                }
+            }
+        }
+    } // End loop over h
+
+    outfile->Printf("\n TPDM:");
+    for (int p = 0; p < ncmo_; ++p) {
+        for (int q = 0; q <= p; ++q) {
+            for (int r = 0; r < ncmo_; ++r) {
+                for (int s = 0; s <= r; ++s) {
+                    if (std::fabs(rdm[tei_index(p,q,r,s)]) > 1.0e-12){
+                        outfile->Printf("\n %+-20.12f : %3d %3d %3d %3d",rdm[tei_index(p,q,r,s)],p,q,r,s);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+/**
+ * Compute the ab two-particle density matrix for a given wave function
+ * @param alfa flag for alfa or beta component, true = aa, false = bb
+ */
+void FCIWfn::compute_2rdm_ab(std::vector<double>& rdm, bool alfa)
+{
+
 }
 
 }}
