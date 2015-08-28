3d45b0d218365797c8158418afb2fff1c901b4ad
Francesco Evangelista <francesco.evangelista@gmail.com>
2015-08-28 10:15:57 -0400

    Changed libadative() -> forte().

--- main.cc
+++ main.cc
@@ -451,7 +451,7 @@ read_options(std::string name, Options &options)
 }
 
 extern "C" PsiReturnType
-libadaptive(Options &options)
+forte(Options &options)
 {
     Timer overall_time;
     ambit::initialize();
