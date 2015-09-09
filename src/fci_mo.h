c3ce672a48103e988f9830022ba310eca2edc805
Jeff Schriber <jbschri@emory.edu>
2015-09-09 15:50:49 -0400

    Begin fix of FCI_MO so that it will compile. Complete construction of FCIIntegral is needed

--- src/fci_mo.h
+++ src/fci_mo.h
@@ -31,7 +31,7 @@ namespace psi{ namespace forte{
 class FCI_MO
 {
 public:
-    FCI_MO(Options &options, ForteIntegrals *ints, std::shared_ptr<MOSpaceInfo> mo_space_info);
+    FCI_MO(Options &options, ForteIntegrals* ints, std::shared_ptr<MOSpaceInfo> mo_space_info);
     ~FCI_MO();
 
     Reference reference();
@@ -44,9 +44,9 @@ protected:
     void cleanup();
 
     // Integrals
-    ForteIntegrals *integral_;
+    ForteIntegrals* integral_;
     std::string int_type_;
-
+	std::shared_ptr<FCIIntegrals> fci_ints_;
     // Reference Type
     std::string ref_type_;
 
