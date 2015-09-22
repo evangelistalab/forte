1a9a0f0ba5e95e61960a7815cd1c2a261161e528
Francesco Evangelista <francesco.evangelista@gmail.com>
2015-09-22 17:54:36 -0400

    Add nice test summary.

--- tests/methods/run_forte_tests.py
+++ tests/methods/run_forte_tests.py
@@ -28,7 +28,7 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2:
     psi4command = sys.argv[1]
 
-print "Running test using psi4 executable found in:\n%s" % psi4command
+print "Running forte tests using the psi4 executable found in:\n  %s\n" % psi4command
 
 fci_tests = ["fci-1","fci-2","fci-3","fci-4","fci-rdms-1","fci-rdms-2"]
 
@@ -56,28 +56,52 @@ maindir = os.getcwd()
 
 test_results = {}
 for d in tests:
-    print "\nRunning test %s" % d,
+    print "Running test %s" % d.upper()
+
     os.chdir(d)
-    
     successful = True
     # Run psi
     try:
         out = subprocess.check_output([psi4command])
-#        print out
     except:
+        # something went wrong
         successful = False
-        test_results[d] = False
+        test_results[d] = "DOES NOT MATCH"
 
     if successful:
         # Check if FORTE ended successfully
         timing = open("output.dat").read()
         m = timing_re.search(timing)
-        message = ""
         if m:
-            message =  bcolors.OKGREEN + "OK" + bcolors.ENDC
+            test_results[d] = "PASSED"
         else:
-            message = bcolors.FAIL + "FAILED" + bcolors.ENDC
-        filler = " " * (66)
-        print "        %s" % (message)
+            test_results[d] = "FAILED"
         print out
     os.chdir(maindir)
+
+summary = []
+nfailed = 0
+nnomatch = 0
+for d in tests:
+    if test_results[d] == "PASSED":
+        msg = bcolors.OKGREEN + "PASSED" + bcolors.ENDC
+    elif test_results[d] == "FAILED":
+        msg = bcolors.FAIL + "FAILED" + bcolors.ENDC
+        nfailed += 1
+    elif test_results[d] == "DOES NOT MATCH":
+        msg = bcolors.FAIL + "DOES NOT MATCH" + bcolors.ENDC
+        nnomatch += 1
+
+    filler = " " * (81 - len(d + msg))
+    summary.append("        %s%s%s" % (d.upper(),filler,msg))
+
+print "Summary:"
+print " " * 8 + "-" * 72
+print "\n".join(summary)
+print " " * 8 + "-" * 72
+
+
+if nnomatch + nfailed == 0:
+    print "Tests: All passed\n"
+else:
+    print "Tests: %d passed, %d failed, %d did not match\n" % (len(tests) -  nnomatch - nfailed,nfailed,nnomatch)
