212c69f77702bd4df43c34e99301a6a7d6fec251
York <lcy@uga.edu>
2015-10-07 00:34:29 -0400

    added fully decoupled cv and ccvv, and cc-type dsrg transformation

--- src/main.cc
+++ src/main.cc
@@ -88,7 +88,7 @@ read_options(std::string name, Options &options)
         options.add_str("JOB_TYPE","EXPLORER","EXPLORER ACI ACI_SPARSE FCIQMC APICI FCI CAS"
                                               " SR-DSRG SR-DSRG-ACI SR-DSRG-APICI TENSORSRG TENSORSRG-CI"
                                               " DSRG-MRPT2 MR-DSRG-PT2 THREE-DSRG-MRPT2 SQ NONE"
-                                              " SOMRDSRG");
+                                              " SOMRDSRG MRDSRG");
 
         /*- The symmetry of the electronic state. (zero based) -*/
         options.add_int("ROOT_SYM",0);
@@ -433,6 +433,8 @@ read_options(std::string name, Options &options)
         options.add_int("NTAMP", 15);
         /*- T Threshold for Intruder States -*/
         options.add_double("INTRUDER_TAMP", 0.10);
+        /*- DSRG Transformation Type -*/
+        options.add_str("DSRG_TRANS_TYPE", "UNITARY", "UNITARY CC");
         /*- DSRG Perturbation -*/
         options.add_bool("DSRGPT", true);
         /*- Zero T1 Amplitudes -*/
@@ -528,6 +530,9 @@ extern "C" PsiReturnType forte(Options &options)
         if(options.get_str("RELAX_REF") == "NONE"){
             mrdsrg->compute_energy();
         }else{
+            if(options.get_str("DSRG_TRANS_TYPE") == "CC"){
+                throw PSIEXCEPTION("Reference relaxation for CC-type DSRG transformation is not implemented yet.");
+            }
             mrdsrg->compute_energy_relaxed();
         }
     }
