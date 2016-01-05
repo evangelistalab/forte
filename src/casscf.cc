09e29c380e0abeddbb7c421a6081e752c91e85b8
Kevin Hannon <kannon1992@gmail.com>
2016-01-05 10:37:44 -0500

    Most test cases for CASSCF pass

--- src/casscf.cc
+++ src/casscf.cc
@@ -160,6 +160,7 @@ void CASSCF::compute_casscf()
         tei_paaa_ = transform_integrals();
         if(casscf_debug_print_){outfile->Printf("\n\n TransInts: %8.8f", my_trans.get());}
 
+
         std::string diis_start_label = "";
         if(iter >= diis_start && do_diis==true && g_norm < 1e-4){diis_start_label = "DIIS";}
         outfile->Printf("\n %4d   %10.12f   %10.12f   %10.12f   %4s", iter, g_norm, fabs(E_casscf_ - E_casscf_old), E_casscf_, diis_start_label.c_str());
