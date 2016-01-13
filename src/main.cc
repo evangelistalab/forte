007e409777c4b0786dfd40d8e7fd8dddda683561
Tianyuan Zhang <tianyuanzhang@c-ac-26-115.wireless.emory.edu>
2016-01-13 12:17:27 -0500

    added an optional option of initial guess threshold

--- src/main.cc
+++ src/main.cc
@@ -386,7 +386,7 @@ read_options(std::string name, Options &options)
         /*- The maximum number of determinants used to form the guess wave function -*/
         options.add_double("MAX_GUESS_SIZE",10000);
         /*- The determinant importance threshold -*/
-        options.add_double("GUESS_SPAWNING_THRESHOLD",0.01);
+        options.add_double("GUESS_SPAWNING_THRESHOLD",-1);
         /*- The threshold with which we estimate the variational energy.
             Note that the final energy is always estimated exactly. -*/
         options.add_double("ENERGY_ESTIMATE_THRESHOLD",1.0e-6);
