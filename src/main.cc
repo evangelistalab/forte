7112aba3503d132a394239efed24733a3ad902bd
York <lcy@uga.edu>
2016-05-19 19:37:26 -0400

    changed mrdsrg-cepa0 to mrdsrg-ldsrg2-qc

--- src/main.cc
+++ src/main.cc
@@ -638,7 +638,7 @@ read_options(std::string name, Options &options)
         ///              OPTIONS FOR THE MR-DSRG MODULE
         //////////////////////////////////////////////////////////////
         /*- Correlation level -*/
-        options.add_str("CORR_LEVEL", "PT2", "LDSRG2 QDSRG2 LDSRG2_P3 QDSRG2_P3 PT2 PT3 CEPA0");
+        options.add_str("CORR_LEVEL", "PT2", "LDSRG2 QDSRG2 LDSRG2_P3 QDSRG2_P3 PT2 PT3 LDSRG2_QC");
         /*- Source Operator -*/
         options.add_str("SOURCE", "STANDARD", "STANDARD LABS DYSON AMP EMP2 LAMP LEMP2");
         /*- The Algorithm to Form T Amplitudes -*/
