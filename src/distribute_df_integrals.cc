7dc576ff8c425506672d10ff789bb2d6eb227156
Kevin Hannon <kannon1992@gmail.com>
2016-08-11 15:40:50 -0400

    CASSCF works in parallel again.  Added parallel casscf test case, and distributed df has a testing function

--- src/distribute_df_integrals.cc
+++ src/distribute_df_integrals.cc
@@ -49,10 +49,12 @@ std::shared_ptr<MOSpaceInfo> mo_space_info)
         my_proc = GA_Nodeid();
     #endif
 
-    if(my_proc == 0) gather_integrals();
+    gather_integrals();
     MPI_Bcast(&nthree_, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    create_dist_df();
-    test_distributed_integrals();
+    if(my_proc == 0)
+    {
+        test_distributed_integrals();
+    }
     //make_diagonal_integrals();
     if (ncmo_ < nmo_){
         if(my_proc == 0) freeze_core_orbitals();
@@ -60,14 +62,14 @@ std::shared_ptr<MOSpaceInfo> mo_space_info)
         aptei_idx_ = ncmo_;
     }
 
-    outfile->Printf("\n  DISKDFIntegrals take %15.8f s", DFInt.get());
+    outfile->Printf("\n  DistDFIntegrals take %15.8f s", DFInt.get());
 }
 
 DistDFIntegrals::~DistDFIntegrals()
 {
     deallocate();
 }
-void DistDFIntegrals::gather_integrals()
+void DistDFIntegrals::test_distributed_integrals()
 {
         outfile->Printf("\n Computing Density fitted integrals \n");
 
@@ -136,38 +138,43 @@ void DistDFIntegrals::gather_integrals()
         boost::shared_ptr<Tensor> B = df->ints()["B"];
         B_ = B;
         df.reset();
-}
-void DistDFIntegrals::create_dist_df()
-{
-    int dim[2], chunk[2];
-    dim[0] = nthree_;
-    dim[1] = nmo_ * nmo_;
-    chunk[0] = -1;
-    chunk[1] = nmo_ * nmo_;
+        int dim[2], chunk[2];
+        dim[0] = nthree_;
+        dim[1] = nmo_ * nmo_;
+        chunk[0] = -1;
+        chunk[1] = nmo_ * nmo_;
 
-    DistDF_ga_ = NGA_Create(C_DBL, 2, dim, (char *)"DistributedDF", chunk);
-    if(DistDF_ga_ == 0)
-        GA_Error("DistributedDF failed on creating the tensor", 1);
-    GA_Print_distribution(DistDF_ga_);
-    int my_proc = GA_Nodeid();
-    int num_proc = GA_Nnodes();
-    if(my_proc == 0)
-    {
-        for(int iproc = 0; iproc < num_proc; iproc++)
+        int MY_DF = NGA_Create(C_DBL, 2, dim, (char *)"DistributedDF", chunk);
+        if(MY_DF == 0)
+            GA_Error("DistributedDF failed on creating the tensor", 1);
+        GA_Print_distribution(MY_DF);
+        int my_proc = GA_Nodeid();
+        int num_proc = GA_Nnodes();
+        if(my_proc == 0)
         {
-            int begin_offset[2];
-            int end_offset[2];
-            int stride[1];
-            stride[0] = nmo_ * nmo_;
-            NGA_Distribution(DistDF_ga_, iproc, begin_offset, end_offset);
-            std::vector<int> begin_offset_vec = {begin_offset[0], begin_offset[1]};
-            std::vector<int> end_offset_vec = {end_offset[0], end_offset[1]};
-            ambit::Tensor B_per_process = read_integral_chunk(B_, begin_offset_vec, end_offset_vec);
-            NGA_Put(DistDF_ga_, begin_offset, end_offset, &(B_per_process.data()[0]), stride);
+            for(int iproc = 0; iproc < num_proc; iproc++)
+            {
+                int begin_offset[2];
+                int end_offset[2];
+                int stride[1];
+                stride[0] = nmo_ * nmo_;
+                NGA_Distribution(MY_DF, iproc, begin_offset, end_offset);
+                std::vector<int> begin_offset_vec = {begin_offset[0], begin_offset[1]};
+                std::vector<int> end_offset_vec = {end_offset[0], end_offset[1]};
+                ambit::Tensor B_per_process = read_integral_chunk(B_, begin_offset_vec, end_offset_vec);
+                NGA_Put(MY_DF, begin_offset, end_offset, &(B_per_process.data()[0]), stride);
+            }
         }
-    }
-    GA_Print_distribution(DistDF_ga_);
-    GA_Print(DistDF_ga_);
+        double positive_one = 1.0;
+        double negative_one = -1.0;
+        GA_Add(&positive_one, MY_DF, &negative_one,DistDF_ga_ , MY_DF);
+        std::vector<double> my_df_norm(2, 0.0);
+        GA_Norm_infinity(MY_DF, &my_df_norm[0]);
+        for(auto my_norm : my_df_norm)
+        {
+            outfile->Printf("\n ||SERIAL_DF - DistDF||_{\infinity} = %4.16f", my_norm);
+        }
+       
 }
 
 ambit::Tensor DistDFIntegrals::read_integral_chunk(boost::shared_ptr<Tensor>& B,std::vector<int>& lo, std::vector<int>& hi)
@@ -193,16 +200,16 @@ ambit::Tensor DistDFIntegrals::read_integral_chunk(boost::shared_ptr<Tensor>& B,
     std::vector<size_t> p_map(nmo_);
     std::vector<size_t> q_map(nmo_);
 
-    for(size_t p_block : all_mos)
-    {
-        p_map[p_block] = p_idx;
-        p_idx++;
-    }
-    for(size_t q_block : all_mos)
-    {
-        q_map[q_block] = q_idx;
-        q_idx++;
-    }
+    //for(size_t p_block : all_mos)
+    //{
+    //    p_map[p_block] = p_idx;
+    //    p_idx++;
+    //}
+    //for(size_t q_block : all_mos)
+    //{
+    //    q_map[q_block] = q_idx;
+    //    q_idx++;
+    //}
 
     for(size_t p_block = 0; p_block < nmo_; p_block++)
     {
@@ -230,7 +237,7 @@ ambit::Tensor DistDFIntegrals::read_integral_chunk(boost::shared_ptr<Tensor>& B,
 }
 void DistDFIntegrals::deallocate()
 {
-    GA_Destroy(DistDF_ga_);
+    //GA_Destroy(DistDF_ga_);
     delete[] diagonal_aphys_tei_aa;
     delete[] diagonal_aphys_tei_ab;
     delete[] diagonal_aphys_tei_bb;
@@ -240,9 +247,6 @@ void DistDFIntegrals::allocate()
     diagonal_aphys_tei_aa = new double[nmo_ * nmo_];
     diagonal_aphys_tei_ab = new double[nmo_ * nmo_];
     diagonal_aphys_tei_bb = new double[nmo_ * nmo_];
-}
-void test_distributed_df_integrals()
-{
 }
 //double DistDFIntegrals::aptei_aa(size_t p, size_t q, size_t r, size_t s)
 //{
@@ -280,14 +284,13 @@ ambit::Tensor DistDFIntegrals::three_integral_block(const std::vector<size_t>& A
     //subscript_end[1] = nmo_ * nmo_;
     //buffer.resize(buffer_size);
     //NGA_Get(DistDF_ga_, one_core_offset_begin, one_core_offset_end, &buffer[0], ld);
-    exit(1);
 
     return ReturnTensor;
 
 }
-void DistDFIntegrals::test_distributed_integrals()
+void DistDFIntegrals::gather_integrals()
 {
-    boost::shared_ptr<BasisSet> auxiliary = BasisSet::pyconstruct_orbital(wfn_->molecule(), "DF_BASIS_SCF",options_.get_str("DF_BASIS_SCF"));
+    boost::shared_ptr<BasisSet> auxiliary = BasisSet::pyconstruct_orbital(wfn_->molecule(), "DF_BASIS_MP2",options_.get_str("DF_BASIS_MP2"));
     int naux = auxiliary->nbf();
     SharedMatrix Ca = wfn_->Ca();
     SharedMatrix Ca_ao(new Matrix("CA_AO", wfn_->nso(), wfn_->nmo()));
@@ -309,12 +312,7 @@ void DistDFIntegrals::test_distributed_integrals()
     ParallelDFMO DFMO = ParallelDFMO(wfn_->basisset(), auxiliary);
     DFMO.set_C(Ca_ao);
     DFMO.compute_integrals();
-    int MY_DF = DFMO.Q_PQ();
-    int test_ga = 0;
-    GA_Duplicate(test_ga, (char *) "ZeroArray");
-    //GA_Print(MY_DF);
-    GA_Print(MY_DF);
-    GA_Print(DistDF_ga_);
+    DistDF_ga_ = DFMO.Q_PQ();
 }
 
 
