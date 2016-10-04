a96486bcfbf5ad14e119c9e197f8dea371511e35
Tianyuan Zhang <tianyuanzhang@n-ac-89-134.wireless.emory.edu>
2016-10-04 10:55:34 -0400

    Started StaticLinkedHash class

---
+++ src/static_linked_hash.cc
@@ -0,0 +1,27 @@
+#include "static_linked_hash.h"
+
+namespace psi{ namespace forte{
+
+template<
+        class Key,
+        class T,
+        class Hash,
+        class KeyEqual,
+        class Allocator
+> StaticLinkedHash<Key, T, Hash, KeyEqual, Allocator>::StaticLinkedHash()
+{
+
+}
+
+template<
+        class Key,
+        class T,
+        class Hash,
+        class KeyEqual,
+        class Allocator
+> StaticLinkedHash<Key, T, Hash, KeyEqual, Allocator>::~StaticLinkedHash()
+{
+
+}
+
+}}
