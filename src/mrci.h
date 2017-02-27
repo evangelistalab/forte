aeed2c2b1045be08894db869a53f1278bb706781
Jeff Schriber <jbschri@emory.edu>
2017-02-26 20:31:58 -0500

    Add the mrci files

---
+++ src/mrci.h
@@ -0,0 +1,73 @@
+
+
+/*
+ * @BEGIN LICENSE
+ *
+ * Forte: an open-source plugin to Psi4 (https://github.com/psi4/psi4)
+ * that implements a variety of quantum chemistry methods for strongly
+ * correlated electrons.
+ *
+ * Copyright (c) 2012-2017 by its authors (see LICENSE, AUTHORS).
+ *
+ * The copyrights for code used from other parties are included in
+ * the corresponding files.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * @END LICENSE
+ */
+
+#include "psi4/libmints/molecule.h"
+#include "psi4/libmints/wavefunction.h"
+#include "psi4/liboptions/liboptions.h"
+
+#include "ci_rdms.h"
+#include "helpers.h"
+#include "reference.h"
+#include "stl_bitset_determinant.h"
+#include "integrals/integrals.h"
+#include "fci/fci_integrals.h"
+#include "determinant_map.h"
+#include "operator.h"
+#include "sparse_ci_solver.h"
+
+namespace psi { namespace forte {
+
+class MRCI : public Wavefunction {
+  public:
+
+    //Class constructor and destructor
+    MRCI( SharedWavefunction ref_wfn, Options& options,
+        std::shared_ptr<ForteIntegrals> ints, 
+        std::shared_ptr<MOSpaceInfo> mo_space_info,
+        DeterminantMap& reference);
+    
+    ~MRCI();
+
+    std::shared_ptr<ForteIntegrals> ints_;
+    DeterminantMap& reference_;
+    std::shared_ptr<MOSpaceInfo> mo_space_info_; 
+
+    double compute_energy(); 
+
+  private:
+    std::shared_ptr<FCIIntegrals> fci_ints_;
+
+    void startup();
+
+    int nroot_;
+    int multiplicity_;
+    DiagonalizationMethod diag_method_;
+};
+}}
