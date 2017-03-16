2fac43d5d1e68d2b84d0e30cf2389c16cedfd1ae
tyzhang1993 <tianyuanzhang1993@gmail.com>
2017-03-16 13:23:01 -0400

    Optimized CIHash

--- src/cihash.h
+++ src/cihash.h
@@ -41,6 +41,7 @@ template <class Key, class Hash = std::hash<Key>> class CIHash {
     std::vector<size_t> begin_index;
     const size_t MIN_NUM_BUCKET = 1 << 0;
     float max_load = 1.0;
+    size_t current_max_load_size = 1;
     const size_t MAX_SIZE = std::min(vec.max_size(), begin_index.max_size());
     size_t num_bucket;
     size_t current_size;
@@ -48,6 +49,7 @@ template <class Key, class Hash = std::hash<Key>> class CIHash {
     std::tuple<size_t, size_t, size_t> find_detail_by_key(const Key& key) const;
     void double_buckets();
     void half_buckets();
+    inline void update_current_max_load_size();
 
   public:
     class iterator;
@@ -101,4 +103,4 @@ template <class Key, class Hash = std::hash<Key>> class CIHash {
     };
 };
 
-#endif // CIHASH_H
+#endif // _cihash_h_
