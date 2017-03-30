1246ed853c30c3fece6cd946df3fc93566fa12e7
Jeff Schriber <jbschri@emory.edu>
2017-03-29 23:31:36 -0400

    Fix higher order RDMs

--- src/aci/aci.cc
+++ src/aci/aci.cc
@@ -717,6 +717,11 @@ double AdaptiveCI::compute_energy() {
         outfile->Printf("\n  Size of approx: %zu  size of var: %zu", approx.size(), final_wfn_.size());
         compute_rdms(approx, op_, new_evecs, 0,0); 
     } else {
+        
+        op_.clear_op_s_lists();
+        op_.clear_tp_s_lists();
+        op_.op_lists(final_wfn_);
+        op_.tp_lists(final_wfn_);
         compute_rdms(final_wfn_, op_, PQ_evecs, 0, 0);
     }
 
