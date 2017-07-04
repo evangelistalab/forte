205a5c407cf7355dffd0abd53175f75c013e3d59
Francesco Evangelista <francesco.evangelista@gmail.com>
2017-07-04 15:23:40 -0400

    Merge.

--- src/pci/pci_hashvec.cc
+++ src/pci/pci_hashvec.cc
@@ -355,7 +355,6 @@ void ProjectorCI_HashVec::print_info() {
     for (auto& str_dim : calculation_info_string) {
         outfile->Printf("\n    %-39s %10s", str_dim.first.c_str(), str_dim.second.c_str());
     }
-    
 }
 
 double ProjectorCI_HashVec::estimate_high_energy() {
@@ -760,7 +759,6 @@ double ProjectorCI_HashVec::compute_energy() {
             }
         }
         beta += time_step_;
-        
     }
 
     if (variational_estimate_) {
@@ -787,9 +785,9 @@ double ProjectorCI_HashVec::compute_energy() {
     outfile->Printf("\n  * Size of CI space                    = %zu", C.size());
     outfile->Printf("\n  * Projector-CI Approximate Energy     = %18.12f Eh", 1, approx_energy_);
 
-    timer_on("PCI:sort");
-    sortHashVecByCoefficient(dets_hashvec, C);
-    timer_off("PCI:sort");
+    //    timer_on("PCI:sort");
+    //    sortHashVecByCoefficient(dets_hashvec, C);
+    //    timer_off("PCI:sort");
     //    outfile->Printf("\nSuccessfully sorted!");
 
     timer_on("PCI:<E>end_v");
@@ -812,7 +810,6 @@ double ProjectorCI_HashVec::compute_energy() {
                     var_energy - approx_energy_);
 
     outfile->Printf("\n\n  %s: %f s", "Projector-CI (bitset) ran in  ", t_apici.elapsed());
-    
 
     if (print_full_wavefunction_) {
         print_wfn(dets_hashvec, C, C.size());
@@ -2309,8 +2306,6 @@ void ProjectorCI_HashVec::print_wfn(const det_hashvec& space_hashvec, std::vecto
     outfile->Printf("\n\n  Spin State: S^2 = %5.3f, S = %5.3f, %s (from %zu "
                     "determinants,%.2f\%)",
                     S2, S, state_label.c_str(), max_I, 100.0 * sum_weight);
-
-    
 }
 
 void ProjectorCI_HashVec::save_wfn(
