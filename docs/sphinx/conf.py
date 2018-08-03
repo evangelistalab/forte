62d2fcd937b315a98d5549c8f2979a6c410128db
Francesco Evangelista <francesco.evangelista@gmail.com>
2018-08-03 10:13:49 -0400

    Changes to docs

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -83,7 +83,14 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx_rtd_theme'
+
+#html_theme = 'sphinx_rtd_theme'
 html_theme_path = ['_themes', ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
