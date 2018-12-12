4ea9dca9803c921fa4f738cd086cfd330a21a0d3
tyzhang1993 <tianyuanzhang1993@gmail.com>
2018-12-12 16:09:51 -0500

    Switch Timer to local_timer in orbitaloptimizer.cc

--- src/operator.cc
+++ src/operator.cc
@@ -404,7 +404,7 @@ double WFNOperator::s2(DeterminantHashVec& wfn, SharedMatrix& evecs, int root) {
     return S2;
 }
 
-double WFNOperator::s2_direct(DeterminantHashVec& wfn, SharedMatrix& evecs, int root ){
+double WFNOperator::s2_direct(DeterminantHashVec& wfn, SharedMatrix& evecs, int root) {
     double S2 = 0.0;
     const det_hashvec& wfn_map = wfn.wfn_hash();
 
@@ -419,7 +419,6 @@ double WFNOperator::s2_direct(DeterminantHashVec& wfn, SharedMatrix& evecs, int 
         S2 += (ms * ms + ms + static_cast<double>(nb) - static_cast<double>(npair)) *
               evecs->get(i, root) * evecs->get(i, root);
     }
-        
 
     // abab contribution
     SortedStringList_UI64 a_sorted_string_list(wfn, fci_ints_, DetSpinType::Alpha);
@@ -429,16 +428,16 @@ double WFNOperator::s2_direct(DeterminantHashVec& wfn, SharedMatrix& evecs, int 
     UI64Determinant::bit_t Ib;
     UI64Determinant::bit_t Jb;
     UI64Determinant::bit_t IJb;
-    
-    for( const auto& detIa : sorted_half_dets){
+
+    for (const auto& detIa : sorted_half_dets) {
         const auto& range_I = a_sorted_string_list.range(detIa);
         size_t first_I = range_I.first;
         size_t last_I = range_I.second;
 
-        for( const auto& detJa : sorted_half_dets){
+        for (const auto& detJa : sorted_half_dets) {
             detIJa_common = detIa ^ detJa;
             int ndiff = ui64_bit_count(detIJa_common);
-            if( ndiff == 2 ){
+            if (ndiff == 2) {
                 int i, a;
                 for (int p = 0; p < ncmo_; ++p) {
                     const bool la_p = ui64_get_bit(detIa, p);
@@ -454,7 +453,7 @@ double WFNOperator::s2_direct(DeterminantHashVec& wfn, SharedMatrix& evecs, int 
                 size_t last_J = range_J.second;
                 for (size_t posI = first_I; posI < last_I; ++posI) {
                     Ib = sorted_dets[posI].get_beta_bits();
-                    double CI = evecs->get(a_sorted_string_list.add(posI),root);
+                    double CI = evecs->get(a_sorted_string_list.add(posI), root);
                     for (size_t posJ = first_J; posJ < last_J; ++posJ) {
                         Jb = sorted_dets[posJ].get_beta_bits();
                         IJb = Jb ^ Ib;
@@ -464,9 +463,9 @@ double WFNOperator::s2_direct(DeterminantHashVec& wfn, SharedMatrix& evecs, int 
                             auto j = lowest_one_idx(Ib_sub);
                             auto Jb_sub = Jb & IJb;
                             auto b = lowest_one_idx(Jb_sub);
-                            if ( (i!=j) and (a != b) and (i == b) and (j == a)) {
-                                double sign = sign_ia * ui64_slater_sign(Ib,j,b);
-                                S2 -= sign * CI * evecs->get(a_sorted_string_list.add(posJ), root);                 
+                            if ((i != j) and (a != b) and (i == b) and (j == a)) {
+                                double sign = sign_ia * ui64_slater_sign(Ib, j, b);
+                                S2 -= sign * CI * evecs->get(a_sorted_string_list.add(posJ), root);
                             }
                         }
                     }
@@ -477,7 +476,6 @@ double WFNOperator::s2_direct(DeterminantHashVec& wfn, SharedMatrix& evecs, int 
 
     return std::fabs(S2);
 }
-
 
 void WFNOperator::add_singles(DeterminantHashVec& wfn) {
     const det_hashvec& wfn_map = wfn.wfn_hash();
@@ -700,11 +698,10 @@ void WFNOperator::build_strings(DeterminantHashVec& wfn) {
 void WFNOperator::op_s_lists(DeterminantHashVec& wfn) {
     timer ops("Single sub. lists");
 
-    if( !quiet_ ){
+    if (!quiet_) {
         print_h2("Computing Coupling Lists");
         outfile->Printf("  --------------------------------");
     }
-
 
     // Get a reference to the determinants
     const det_hashvec& dets = wfn.wfn_hash();
