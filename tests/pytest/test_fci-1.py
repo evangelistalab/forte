52b8714310488bb0d75d6a7e9bbea44f620d4665
tyzhang1993 <tianyuanzhang1993@gmail.com>
2018-12-18 14:21:43 -0500

    Solved the option not available issue.

--- tests/pytest/test_fci-1.py
+++ tests/pytest/test_fci-1.py
@@ -25,6 +25,7 @@ def test_fci1():
     mo_space_info = forte.make_mo_space_info(wfn, options)    
     ints = forte.make_forte_integrals(wfn, options, mo_space_info)
 #    solver = forte.FCI(state,forte_options,ints,mo_space_info)
+    forte_options = forte.ForteOptions(options)
     solver = forte.make_active_space_solver('FCI',state,forte_options,ints,mo_space_info)
     energy = solver.compute_energy()
 
