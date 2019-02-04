fda37902873d70041bcddc5eec598c1799bcebd7
Nan <foreceed@hotmail.com>
2019-02-04 15:19:22 -0500

    Add Nan and Shuhe's changes

--- register_forte_options.py
+++ register_forte_options.py
@@ -142,95 +142,74 @@ def register_aci_options(forte_options):
     forte_options.add_double("ACI_NO_THRESHOLD", 0.02, "Threshold for active space prediction")
     forte_options.add_double("ACI_SPIN_TOL", 0.02, "Tolerance for S^2 value")
 
-    /*- Approximate 1RDM? -*/
-    foptions.add_bool("ACI_APPROXIMATE_RDM", false, "Approximate the RDMs");
-    /*- Test RDMs -*/
-    foptions.add_bool("ACI_TEST_RDMS", false, "Run test for the RDMs");
+    # /*- Approximate 1RDM? -*/
+    forte_options.add_bool("ACI_APPROXIMATE_RDM", false, "Approximate the RDMs")
+    forte_options.add_bool("ACI_TEST_RDMS", false, "Run test for the RDMs")
+    forte_options.add_bool("ACI_FIRST_ITER_ROOTS", false, "Compute all roots on first iteration?")
+    forte_options.add_bool("ACI_PRINT_WEIGHTS", false, "Print weights for active space prediction")
 
-    /*- Do compute nroots on first cycle? -*/
-    foptions.add_bool("ACI_FIRST_ITER_ROOTS", false, "Compute all roots on first iteration?");
-    foptions.add_bool("ACI_PRINT_WEIGHTS", false, "Print weights for active space prediction");
+    forte_options.add_bool("ACI_PRINT_NO", true, "Print the natural orbitals")
 
-    /*- Print Natural orbitals -*/
-    foptions.add_bool("ACI_PRINT_NO", true, "Print the natural orbitals");
+    forte_options.add_bool("ACI_NO", false, "Computes ACI natural orbitals")
 
-    /*- Compute ACI-NOs -*/
-    foptions.add_bool("ACI_NO", false, "Computes ACI natural orbitals");
+    forte_options.add_bool("MRPT2", false, "Compute full PT2 energy")
 
-    /*- Compute full PT2 energy -*/
-    foptions.add_bool("MRPT2", false, "Compute full PT2 energy");
+    forte_options.add_bool("UNPAIRED_DENSITY", false, "Compute unpaired electron density")
 
-    /*- Compute unpaired electron density -*/
-    foptions.add_bool("UNPAIRED_DENSITY", false, "Compute unpaired electron density");
+    forte_options.add_bool("ACI_ADD_SINGLES", false,
+                      "Adds all active single excitations to the final wave function")
+    forte_options.add_bool("ACI_ADD_EXTERNAL_EXCITATIONS", false,
+                      "Adds external single excitations to the final wave function")
+    forte_options.add_str("ACI_EXTERNAL_EXCITATION_ORDER", "SINGLES",
+                     "Order of external excitations to add")
+    forte_options.add_str("ACI_EXTERNAL_EXCITATION_TYPE", "ALL", "Type of external excitations to add")
+    forte_options.add_bool("ESNOS", false, "Compute external single natural orbitals (ESNO)")
+    forte_options.add_int("ESNO_MAX_SIZE", 0, "Number of external orbitals to correlate")
 
-    /*- Add all active singles -*/
-    foptions.add_bool("ACI_ADD_SINGLES", false,
-                      "Adds all active single excitations to the final wave function");
-    /*- Add all active singles -*/
-    foptions.add_bool("ACI_ADD_EXTERNAL_EXCITATIONS", false,
-                      "Adds external single excitations to the final wave function");
-    /*- Order of external excitations to add -*/
-    foptions.add_str("ACI_EXTERNAL_EXCITATION_ORDER", "SINGLES",
-                     "Order of external excitations to add");
-    /*- Type of external excitations to add -*/
-    foptions.add_str("ACI_EXTERNAL_EXCITATION_TYPE", "ALL", "Type of external excitations to add");
+    forte_options.add_bool("ACI_LOW_MEM_SCREENING", false, "Use low-memory screening algorithm")
 
-    /*- Do ESNO transformation? -*/
-    foptions.add_bool("ESNOS", false, "Compute external single natural orbitals");
-    foptions.add_int("ESNO_MAX_SIZE", 0, "Number of external orbitals to correlate");
+    forte_options.add_bool("ACI_REF_RELAX", false, "Do reference relaxation in ACI")
 
-    /*- optionally use low-memory screening -*/
-    foptions.add_bool("ACI_LOW_MEM_SCREENING", false, "Use low-memory screening algorithm");
+    forte_options.add_str("ACI_EX_TYPE", "CONV", "Type of excited state to compute")
 
-    /*- Do reference relaxation in ACI? -*/
-    foptions.add_bool("ACI_REF_RELAX", false, "Do reference relaxation in ACI");
+    forte_options.add_int("ACI_NFROZEN_CORE", 0, "Number of orbitals to freeze for core excitations")
 
-    /*- Type of excited state to compute -*/
-    foptions.add_str("ACI_EX_TYPE", "CONV", "Type of excited state to compute");
+    forte_options.add_int("ACI_ROOTS_PER_CORE", 1, "Number of roots to compute per frozen orbital")
 
-    /*- Number of core orbitals to freeze -*/
-    foptions.add_int("ACI_NFROZEN_CORE", 0, "Number of orbitals to freeze for core excitations");
+    forte_options.add_bool("ACI_SPIN_ANALYSIS", false, "Do spin correlation analysis")
+    forte_options.add_bool("ACI_RELAXED_SPIN", false,
+                      "Do spin correlation analysis for relaxed wave function")
 
-    /*- Number of roots to compute per frozen orbital -*/
-    foptions.add_int("ACI_ROOTS_PER_CORE", 1, "Number of roots to compute per frozen occupation");
+    forte_options.add_bool("PRINT_IAOS", true, "Print IAOs")
 
-    /*- Do spin analysis? -*/
-    foptions.add_bool("ACI_SPIN_ANALYSIS", false, "Do spin correlation analysis");
-    foptions.add_bool("ACI_RELAXED_SPIN", false,
-                      "Do spin correlation analysis for relaxed wave function");
+    forte_options.add_bool("PI_ACTIVE_SPACE", false, "Active space type")
 
-    /*- Print IAOs -*/
-    foptions.add_bool("PRINT_IAOS", true, "Print IAOs");
+    forte_options.add_bool("SPIN_MAT_TO_FILE", false, "Save spin correlation matrix to file")
 
-    /*- Active Space type -*/
-    foptions.add_bool("PI_ACTIVE_SPACE", false, "Active space type");
+    forte_options.add_bool("ACI_RELAXED_SPIN", false,
+                      "Do spin correlation analysis for relaxed wave function")
 
-    /*- Save spin correlation matrix to file -*/
-    foptions.add_bool("SPIN_MAT_TO_FILE", false, "Save spin correlation matrix to file");
+    forte_options.add_bool("PRINT_IAOS", true, "Print IAOs")
 
-    foptions.add_str("SPIN_BASIS", "LOCAL", "Basis for spin analysis");
+    forte_options.add_bool("PI_ACTIVE_SPACE", false, "Active space type")
 
-    /*- Sigma for reference relaxation -*/
-    foptions.add_double("ACI_RELAX_SIGMA", 0.01, "Sigma for reference relaxation");
+    forte_options.add_bool("SPIN_MAT_TO_FILE", false, "Save spin correlation matrix to file")
 
-    /*- Control batched screeing -*/
-    foptions.add_bool("ACI_BATCHED_SCREENING", false, "Control batched screeing");
+    forte_options.add_str("SPIN_BASIS", "LOCAL", "Basis for spin analysis")
 
-    /*- Number of batches in screening  -*/
-    foptions.add_int("ACI_NBATCH", 1, "Number of batches in screening");
+    forte_options.add_double("ACI_RELAX_SIGMA", 0.01, "Sigma for reference relaxation")
 
-    /*- Sets max memory for batching algorithm (MB) -*/
-    foptions.add_int("ACI_MAX_MEM", 1000, "Sets max memory for batching algorithm (MB)");
+    forte_options.add_bool("ACI_BATCHED_SCREENING", false, "Control batched screeing")
 
-    /*- Scales sigma in batched algorithm -*/
-    foptions.add_double("ACI_SCALE_SIGMA", 0.5, "Scales sigma in batched algorithm");
+    forte_options.add_int("ACI_NBATCH", 1, "Number of batches in screening")
 
-    /*- Computes RDMs without coupling lists -*/
-    foptions.add_bool("ACI_DIRECT_RDMS", false, "Computes RDMs without coupling lists");
+    forte_options.add_int("ACI_MAX_MEM", 1000, "Sets max memory for batching algorithm (MB)")
 
-    // temp
-    foptions.add_str("ACI_BATCH_ALG", "HASH", "Algorithm to use for batching");
+    forte_options.add_double("ACI_SCALE_SIGMA", 0.5, "Scales sigma in batched algorithm")
 
+    forte_options.add_bool("ACI_DIRECT_RDMS", false, "Computes RDMs without coupling lists")
+
+    forte_options.add_str("ACI_BATCH_ALG", "HASH", "Algorithm to use for batching")
 
 def register_integral_options(forte_options):
     forte_options.add_str(
