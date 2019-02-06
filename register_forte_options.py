ec4898f9d792d6fa2fa251d63429a9d32d41f41a
fevangelista <francesco.evangelista@emory.edu>
2019-02-05 20:19:54 -0500

    Update register_forte_options.py

--- register_forte_options.py
+++ register_forte_options.py
@@ -28,61 +28,47 @@ def register_driver_options(forte_options):
 def register_avas_options(forte_options):
     forte_options.add_double("AVAS_SIGMA", 0.98, "Threshold that controls the size of the active space")
     forte_options.add_int("AVAS_NUM_ACTIVE", 0,
-                     "Allows the user to specify the "
-                     "total number of active orbitals. "
-                     "It takes priority over the "
+                     "The total number of active orbitals. "
+                     "Takes priority over "
                      "threshold based selection.")
     forte_options.add_int("AVAS_NUM_ACTIVE_OCC", 0,
-                     "Allows the user to specify the "
-                     "number of active occupied orbitals. "
-                     "It takes priority over the "
+                     "The number of active occupied orbitals. "
+                     "Takes priority over "
                      "threshold based selection.")
     forte_options.add_int("AVAS_NUM_ACTIVE_VIR", 0,
-                     "Allows the user to specify the "
-                     "number of active occupied orbitals. "
-                     "It takes priority over the "
+                     "The number of active occupied orbitals. "
+                     "Takes priority over "
                      "threshold based selection.")
     forte_options.add_bool("AVAS_DIAGONALIZE", True,
-                      "Allow the users to specify"
-                      "diagonalization of Socc and Svir"
-                      "It takes priority over the"
+                      "Diagonalize Socc and Svir?"
+                      "This option takes priority over "
                       "threshold based selection.")
 
 def register_cino_options(forte_options):
     forte_options.add_bool("CINO", False, "Do a CINO computation?")
+    
     forte_options.add_str("CINO_TYPE", "CIS", ["CIS", "CISD"], "The type of wave function.")
+    
     forte_options.add_int("CINO_NROOT", 1, "The number of roots computed")
+    
     forte_options.add_array("CINO_ROOTS_PER_IRREP",
-                       "The number of excited states per irreducible representation")
+                       "The number of excited states per irreducible representation")    
     forte_options.add_double("CINO_THRESHOLD", 0.99,
                         "The fraction of NOs to include in the active space")
-#   forte_options.add_int("ACI_MAX_RDM", 1, "Order of RDM to compute
-#     Type of spin projection
-#     * 0 - None
-#     * 1 - Project initial P spaces at each iteration
-#     * 2 - Project only after converged PQ space
-#     * 3 - Do 1 and 2 ")
     forte_options.add_bool("CINO_AUTO", False,
-                      "Allow the users to choose"
-                      "whether pass frozen_docc"
-                      "actice_docc and restricted_docc"
-                      "or not")
+                      "{ass frozen_docc, actice_docc, and restricted_docc?")
 
 def register_mrcino_options(forte_options):
     forte_options.add_bool("MRCINO", False, "Do a MRCINO computation?")
+    
     forte_options.add_str("MRCINO_TYPE", "CIS", ["CIS", "CISD"], "The type of wave function.")
+    
     forte_options.add_int("MRCINO_NROOT", 1, "The number of roots computed")
+    
     forte_options.add_array("MRCINO_ROOTS_PER_IRREP",
                        "The number of excited states per irreducible representation")
     forte_options.add_double("MRCINO_THRESHOLD", 0.99,
                         "The fraction of NOs to include in the active space")
-#    forte_options.add_int("ACI_MAX_RDM", 1, "Order of RDM to compute 
-#     Type of spin projection
-#     * 0 - None
-#     * 1 - Project initial P spaces at each iteration
-#     * 2 - Project only after converged PQ space
-#     * 3 - Do 1 and 2 ")
-
     forte_options.add_bool("MRCINO_AUTO", False,
                       "Allow the users to choose"
                       "whether pass frozen_docc"
