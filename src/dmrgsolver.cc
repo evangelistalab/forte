431a29251a09198065ee3b57f3fa16291127d2af
nstair <nick_stair@yahoo.com>
2019-06-21 18:02:34 -0400

    6

--- src/dmrgsolver.cc
+++ src/dmrgsolver.cc
@@ -568,7 +568,7 @@ void DMRGSolver::compute_energy() {
 
             if(next_site.size() > 1){
                 std::vector<double> rnnn;
-                int previous_site = input_order[input_order.size()-2]
+                int previous_site = input_order[input_order.size()-2];
                 for(auto i : next_site){
                     rnn.push_back(Rij_input_idx->get(previous_site, i));
                     std::cout << "Ri_current_site-1: " << Rij_input_idx->get(previous_site, i) << std::endl;
