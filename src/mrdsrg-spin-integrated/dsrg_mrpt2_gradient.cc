021a03fc475f725fc1c289a4d263be15256ad801
ustcwsh <shuhe.wang@emory.edu>
2019-12-16 15:45:03 -0500

    add framwork & test MCSCF and CASSCF and confirm the consistency

---
+++ src/mrdsrg-spin-integrated/dsrg_mrpt2_gradient.cc
@@ -0,0 +1,183 @@
+/**
+ * DSRG-MRPT2 gradient code by Shuhe Wang
+ *
+ * The computation procedure is listed as belows:
+ * (1), Set MOs spaces;
+ * (2), Set Tensors (F, H, V etc.);
+ * (3), Compute and write the Lagrangian;
+ * (4), Write 1RDMs and 2RDMs coefficients;
+ * (5), Back-transform the TPDM.
+ */
+#include <algorithm>
+#include <math.h>
+#include <numeric>
+#include <ctype.h>
+
+#include "psi4/libpsio/psio.h"
+#include "psi4/libpsio/psio.hpp"
+#include "psi4/libqt/qt.h"
+#include "psi4/libmints/dipole.h"
+
+#include "helpers/timer.h"
+#include "ci_rdm/ci_rdms.h"
+#include "boost/format.hpp"
+#include "sci/fci_mo.h"
+#include "fci/fci_solver.h"
+#include "helpers/printing.h"
+#include "dsrg_mrpt2.h"
+
+#include "psi4/libmints/factory.h"
+#include "psi4/libiwl/iwl.hpp"
+#include "psi4/libpsio/psio.hpp"
+
+#include "gradient_tpdm/backtransform_tpdm.h"
+#include "psi4/libmints/matrix.h"
+#include "psi4/libmints/wavefunction.h"
+#include "psi4/psifiles.h"
+
+using namespace ambit;
+using namespace psi;
+
+namespace forte {
+
+void DSRG_MRPT2::set_all_variables() {
+	// TODO: set global variables for future use.
+	// NOTICE: This function may better be merged into "dsrg_mrpt2.cc" in the future!!
+}
+
+
+
+// set_ambit_space has been defined in "master_mrdsrg.cc"
+// set_density has been defined in "dsrg_mrpt2.cc"
+// set_h has been defined in "dsrg_mrpt2.cc"
+// set_v has been defined in "dsrg_mrpt2.cc"
+// set_fock has been defined in "dsrg_mrpt2.cc"
+
+void DSRG_MRPT2::set_lagrangian() {
+	// TODO: set coefficients before the overlap integral
+}
+
+// It's not necessary to define set_tensor
+
+void DSRG_MRPT2::tpdm_backtransform() {
+	// Backtransform the TPDM
+	// NOTICE: This function also appears in the CASSCF gradient code thus can be refined in the future!!
+    
+    std::vector<std::shared_ptr<psi::MOSpace>> spaces;
+    spaces.push_back(psi::MOSpace::all);
+    std::shared_ptr<TPDMBackTransform> transform =
+        std::shared_ptr<TPDMBackTransform>(new TPDMBackTransform(
+            ints_->wfn(), spaces,
+            IntegralTransform::TransformationType::Unrestricted, // Transformation type
+            IntegralTransform::OutputType::DPDOnly,              // Output buffer
+            IntegralTransform::MOOrdering::QTOrder,              // MO ordering
+            IntegralTransform::FrozenOrbitals::None));           // Frozen orbitals?
+    transform->backtransform_density();
+    transform.reset();
+
+    outfile->Printf("\n    TPDM Backtransformation ......................... Done");
+}
+
+
+SharedMatrix DSRG_MRPT2::compute_gradient() {
+	// TODO: compute the DSRG_MRPT2 gradient 
+    print_method_banner({"DSRG-MRPT2 Gradient", "Shuhe Wang"});
+    set_all_variables();
+    write_lagrangian();
+    write_1rdm_spin_dependent();
+    write_2rdm_spin_dependent();
+    tpdm_backtransform();
+
+    outfile->Printf("\n    Computing Gradient .............................. Done\n");
+    return std::make_shared<Matrix>("nullptr", 0, 0);
+}
+
+
+void DSRG_MRPT2::write_lagrangian() {
+	// TODO: write the Lagrangian
+    outfile->Printf("\n    Writing Lagrangian .............................. ");
+
+    set_lagrangian();
+
+
+    outfile->Printf("Done");
+}
+
+
+
+void DSRG_MRPT2::write_1rdm_spin_dependent() {
+	// TODO: write spin_dependent one-RDMs coefficients. 
+    outfile->Printf("\n    Writing 1RDM Coefficients ....................... ");
+
+
+    outfile->Printf("Done");
+}
+
+
+void DSRG_MRPT2::write_2rdm_spin_dependent() {
+	// TODO: write spin_dependent two-RDMs coefficients using IWL
+    outfile->Printf("\n    Writing 2RDM Coefficients ....................... ");
+
+    auto psio_ = _default_psio_lib_;
+    IWL d2aa(psio_.get(), PSIF_MO_AA_TPDM, 1.0e-14, 0, 0);
+    IWL d2ab(psio_.get(), PSIF_MO_AB_TPDM, 1.0e-14, 0, 0);
+    IWL d2bb(psio_.get(), PSIF_MO_BB_TPDM, 1.0e-14, 0, 0);
+
+
+	// TODO: write coefficients here
+
+    d2aa.flush(1);
+    d2bb.flush(1);
+    d2ab.flush(1);
+
+    d2aa.set_keep_flag(1);
+    d2bb.set_keep_flag(1);
+    d2ab.set_keep_flag(1);
+
+    d2aa.close();
+    d2bb.close();
+    d2ab.close();
+
+    outfile->Printf("Done");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
