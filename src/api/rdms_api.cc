552315cdb6176cddc0623ff53396ae1ace51acd8
Francesco Evangelista <francesco.evangelista@gmail.com>
2019-12-20 16:24:49 -0500

    Remove _np suffix

--- src/api/rdms_api.cc
+++ src/api/rdms_api.cc
@@ -42,55 +42,55 @@ void export_RDMs(py::module& m) {
     py::class_<RDMs>(m, "RDMs")
         .def("max_rdm_level", &RDMs::max_rdm_level, "Return the max RDM level")
         .def(
-            "g1a_np", [](RDMs& rdm) { return ambit_to_np(rdm.g1a()); },
+            "g1a", [](RDMs& rdm) { return ambit_to_np(rdm.g1a()); },
             "Return the alpha 1RDM as a numpy array")
         .def(
-            "g1b_np", [](RDMs& rdm) { return ambit_to_np(rdm.g1b()); },
+            "g1b", [](RDMs& rdm) { return ambit_to_np(rdm.g1b()); },
             "Return the beta 1RDM as a numpy array")
         .def(
-            "g2aa_np", [](RDMs& rdm) { return ambit_to_np(rdm.g2aa()); },
+            "g2aa", [](RDMs& rdm) { return ambit_to_np(rdm.g2aa()); },
             "Return the alpha-alpha 2RDM as a numpy array")
         .def(
-            "g2ab_np", [](RDMs& rdm) { return ambit_to_np(rdm.g2ab()); },
+            "g2ab", [](RDMs& rdm) { return ambit_to_np(rdm.g2ab()); },
             "Return the alpha-beta 2RDM as a numpy array")
         .def(
-            "g2bb_np", [](RDMs& rdm) { return ambit_to_np(rdm.g2bb()); },
+            "g2bb", [](RDMs& rdm) { return ambit_to_np(rdm.g2bb()); },
             "Return the beta-beta 2RDM as a numpy array")
         .def(
-            "g3aaa_np", [](RDMs& rdm) { return ambit_to_np(rdm.g3aaa()); },
+            "g3aaa", [](RDMs& rdm) { return ambit_to_np(rdm.g3aaa()); },
             "Return the alpha-alpha-alpha 3RDM as a numpy array")
         .def(
-            "g3aab_np", [](RDMs& rdm) { return ambit_to_np(rdm.g3aab()); },
+            "g3aab", [](RDMs& rdm) { return ambit_to_np(rdm.g3aab()); },
             "Return the alpha-alpha-beta 3RDM as a numpy array")
         .def(
-            "g3abb_np", [](RDMs& rdm) { return ambit_to_np(rdm.g3abb()); },
+            "g3abb", [](RDMs& rdm) { return ambit_to_np(rdm.g3abb()); },
             "Return the alpha-beta-beta 3RDM as a numpy array")
         .def(
-            "g3bbb_np", [](RDMs& rdm) { return ambit_to_np(rdm.g3bbb()); },
+            "g3bbb", [](RDMs& rdm) { return ambit_to_np(rdm.g3bbb()); },
             "Return the beta-beta-beta 3RDM as a numpy array")
         .def(
-            "SFg2_data_np", [](RDMs& rdm) { return ambit_to_np(rdm.SFg2()); },
+            "SFg2_data", [](RDMs& rdm) { return ambit_to_np(rdm.SFg2()); },
             "Return the spin-free 2-RDM as a numpy array")
         .def(
-            "L2aa_np", [](RDMs& rdm) { return ambit_to_np(rdm.L2aa()); },
+            "L2aa", [](RDMs& rdm) { return ambit_to_np(rdm.L2aa()); },
             "Return the alpha-alpha 2-cumulant as a numpy array")
         .def(
-            "L2ab_np", [](RDMs& rdm) { return ambit_to_np(rdm.L2ab()); },
+            "L2ab", [](RDMs& rdm) { return ambit_to_np(rdm.L2ab()); },
             "Return the alpha-beta 2-cumulant as a numpy array")
         .def(
-            "L2bb_np", [](RDMs& rdm) { return ambit_to_np(rdm.L2bb()); },
+            "L2bb", [](RDMs& rdm) { return ambit_to_np(rdm.L2bb()); },
             "Return the beta-beta 2-cumulant as a numpy array")
         .def(
-            "L3aaa_np", [](RDMs& rdm) { return ambit_to_np(rdm.L3aaa()); },
+            "L3aaa", [](RDMs& rdm) { return ambit_to_np(rdm.L3aaa()); },
             "Return the alpha-alpha-alpha 3-cumulant as a numpy array")
         .def(
-            "L3aab_np", [](RDMs& rdm) { return ambit_to_np(rdm.L3aab()); },
+            "L3aab", [](RDMs& rdm) { return ambit_to_np(rdm.L3aab()); },
             "Return the alpha-alpha-beta 3-cumulant as a numpy array")
         .def(
-            "L3abb_np", [](RDMs& rdm) { return ambit_to_np(rdm.L3abb()); },
+            "L3abb", [](RDMs& rdm) { return ambit_to_np(rdm.L3abb()); },
             "Return the alpha-beta-beta 3-cumulant as a numpy array")
         .def(
-            "L3bbb_np", [](RDMs& rdm) { return ambit_to_np(rdm.L3bbb()); },
+            "L3bbb", [](RDMs& rdm) { return ambit_to_np(rdm.L3bbb()); },
             "Return the beta-beta-beta 3-cumulant as a numpy array");
 }
 } // namespace forte
