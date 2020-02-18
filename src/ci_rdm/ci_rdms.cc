6b134893175a2f22b6c81238509271370d83367d
Francesco Evangelista <francesco.evangelista@gmail.com>
2020-02-18 17:31:20 -0500

    Add missing header

--- src/ci_rdm/ci_rdms.cc
+++ src/ci_rdm/ci_rdms.cc
@@ -30,6 +30,7 @@
 //#include "psi4/libmints/wavefunction.h"
 //#include "psi4/liboptions/liboptions.h"
 //#include "psi4/libpsi4util/process.h"
+#include <cmath>
 
 #include "helpers/timer.h"
 #include "sparse_ci/operator.h"
