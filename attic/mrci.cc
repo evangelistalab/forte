d13736a46a5e06d8166cb9b06859f4c80d3642b8
Francesco Evangelista <francesco.evangelista@gmail.com>
2020-02-22 14:34:57 -0500

    Update copyright year

--- attic/mrci.cc
+++ attic/mrci.cc
@@ -6,7 +6,7 @@
  * that implements a variety of quantum chemistry methods for strongly
  * correlated electrons.
  *
- * Copyright (c) 2012-2019 by its authors (see COPYING, COPYING.LESSER, AUTHORS).
+ * Copyright (c) 2012-2020 by its authors (see COPYING, COPYING.LESSER, AUTHORS).
  *
  * The copyrights for code used from other parties are included in
  * the corresponding files.
--- src/forte_old_methods.cc
+++ src/forte_old_methods.cc
@@ -5,7 +5,7 @@
  * that implements a variety of quantum chemistry methods for strongly
  * correlated electrons.
  *
- * Copyright (c) 2012-2019 by its authors (see COPYING, COPYING.LESSER,
+ * Copyright (c) 2012-2020 by its authors (see COPYING, COPYING.LESSER,
  * AUTHORS).
  *
  * The copyrights for code used from other parties are included in
