c8a66e8a263bf6b5f665d0cbcaba70a906bd5f28
York <lcy@uga.edu>
2020-12-08 16:45:13 +0800

    add a comment

--- options_helpers.py
+++ options_helpers.py
@@ -16,7 +16,11 @@ def reset_forte_options():
     # If we are running psi4, push the options defined in forte_options to psi
     if 'psi4' in sys.modules:
         psi_options = psi4.core.get_options()
-        psi_options.set_current_module('FORTE')  # clean up all Forte module options in Psi4
+        psi_options.set_current_module('FORTE')
+        # TODO: need Psi4 functionality to clean up all Forte module options
+        # something like psi_options.clean_options_module('FORTE')
+        # because the function below only calls `add` functions of Psi4 Options,
+        # which doesn't update option values if the option key is already in the map.
         forte_options.push_options_to_psi4(psi_options)
 
     return forte_options
