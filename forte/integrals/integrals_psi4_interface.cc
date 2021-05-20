ab7dd8b57c110ae8e6e1c5543c10c74cb4ec982d
Meng Huang <35382186+huangm29@users.noreply.github.com>
2021-05-20 15:28:24 -0400

    Adding algorithm header for 6 source files (#234)

--- forte/integrals/integrals_psi4_interface.cc
+++ forte/integrals/integrals_psi4_interface.cc
@@ -25,6 +25,7 @@
  *
  * @END LICENSE
  */
+#include <algorithm>
 
 #include "psi4/psi4-dec.h"
 #include "psi4/libpsi4util/PsiOutStream.h"
--- forte/mrdsrg-spin-integrated/master_mrdsrg.cc
+++ forte/mrdsrg-spin-integrated/master_mrdsrg.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <numeric>
 
 #include "psi4/libpsi4util/PsiOutStream.h"
