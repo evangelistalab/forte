01de8883c49b6950d85c4f0dbff92b3d088787bd
Francesco Evangelista <francesco.evangelista@gmail.com>
2021-06-23 22:12:27 -0400

    Added fci-one-electron test

---
+++ tests/pytest-methods/fci/test_fci-one-electron.py
@@ -0,0 +1,36 @@
+import pytest
+
+from forte.solvers import solver_factory, HF, ActiveSpaceSolver
+
+
+def test_fci_one_electron():
+    """Test FCI on a one-electron system"""
+
+    ref_hf_energy = -0.600480545551890
+    ref_fci_energy = -0.600480545551890
+
+    xyz = """
+    H
+    H 1 1.0
+    """
+    root = solver_factory(molecule=xyz, basis='aug-cc-pVDZ')
+    state = root.state(charge=1, multiplicity=2, sym='ag')
+    hf = HF(root, state=state, e_convergence=1.0e-12)
+    fci = ActiveSpaceSolver(
+        hf,
+        type='FCI',
+        states=state,
+        e_convergence=1.0e-12,
+        frozen_docc=[0, 0, 0, 0, 0, 0, 0, 0],
+        restricted_docc=[0, 0, 0, 0, 0, 0, 0, 0],
+        active=[1, 0, 0, 0, 0, 0, 0, 0]
+    )
+    fci.run()
+
+    # check results
+    assert hf.value('hf energy') == pytest.approx(ref_hf_energy, 1.0e-10)
+    assert fci.value('active space energy')[state] == pytest.approx([ref_fci_energy], 1.0e-10)
+
+
+if __name__ == "__main__":
+    test_fci_one_electron()
