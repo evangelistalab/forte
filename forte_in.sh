1a17b4d5faeb1985038c972f3cbe3a31f9357cca
Meng Huang <huangm1065@gmail.com>
2021-07-02 13:10:00 -0400

    brutal-force cleanup

--- forte_in.sh
+++
@@ -1,4 +0,0 @@
-/home/mhuang/anaconda3/envs/psi4py37/bin/cmake -C /home/mhuang/psi4_install/share/cmake/psi4/psi4PluginCache.cmake -DCMAKE_PREFIX_PATH=/home/mhuang/psi4_install . \
-       	-Dambit_DIR=/home/mhuang/ambit/bin/share/cmake/ambit \
-        -DCMAKE_BUILD_TYPE=Debug	
-#        -DOpenMP_LIBRARY_DIRS=/home/mhuang/anaconda3/lib 
