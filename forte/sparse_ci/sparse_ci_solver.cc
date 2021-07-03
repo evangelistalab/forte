4d605823db402f6296f88ddd8c09390b33112c36
Meng Huang <35382186+huangm29@users.noreply.github.com>
2021-07-03 15:29:24 -0400

    Update forte/sparse_ci/sparse_ci_solver.cc
    Co-authored-by: fevangelista <francesco.evangelista@emory.edu>

--- forte/sparse_ci/sparse_ci_solver.cc
+++ forte/sparse_ci/sparse_ci_solver.cc
@@ -662,7 +662,8 @@ bool SparseCISolver::davidson_liu_solver(const DeterminantHashVec& space,
     }
 
     if (converged != SolverStatus::Converged) {
-        outfile->Printf("\n  The Davidson-Liu algorithm did not converge! Consider to increase DL_MAXITER. ");
+        std::string msg = "\n  The Davidson-Liu algorithm did not converge! Consider increasing the option DL_MAXITER."
+        throw std::runtime_error(msg);
         exit(1);
     }
 
