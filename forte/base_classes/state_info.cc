a08bd5a7483eb5c4534297d114811f945b39fefa
Meng Huang <huangm1065@gmail.com>
2022-06-15 04:57:15 +0800

    Modify comments

--- forte/base_classes/state_info.cc
+++ forte/base_classes/state_info.cc
@@ -69,7 +69,7 @@ const std::vector<size_t>& StateInfo::gas_min() const { return gas_min_; }
 const std::vector<size_t>& StateInfo::gas_max() const { return gas_max_; }
 
 bool StateInfo::operator<(const StateInfo& rhs) const {
-    // Make sure the roots are in correct energy order
+    // Make sure the roots are in increasing energy order for core-excited state calcualtions
     if ((gas_min_ == rhs.gas_min_) && (gas_max_ == rhs.gas_max_)) {
         return std::tie(na_, nb_, multiplicity_, twice_ms_, irrep_) <
                std::tie(rhs.na_, rhs.nb_, rhs.multiplicity_, rhs.twice_ms_, rhs.irrep_);
