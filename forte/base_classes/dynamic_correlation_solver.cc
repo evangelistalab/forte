8c964acfde2d2ad4d2b7dfcffcc55296c9b75e96
fevangelista <francesco.evangelista@emory.edu>
2023-01-16 17:57:31 -0500

    Update year in license

--- forte/base_classes/dynamic_correlation_solver.cc
+++ forte/base_classes/dynamic_correlation_solver.cc
@@ -5,7 +5,7 @@
  * that implements a variety of quantum chemistry methods for strongly
  * correlated electrons.
  *
- * Copyright (c) 2012-2022 by its authors (see COPYING, COPYING.LESSER,
+ * Copyright (c) 2012-2023 by its authors (see COPYING, COPYING.LESSER,
  * AUTHORS).
  *
  * The copyrights for code used from other parties are included in
@@ -85,7 +85,7 @@ double DynamicCorrelationSolver::compute_reference_energy() {
     auto nactv = mo_space_info_->size("ACTIVE");
     auto Fc = ambit::Tensor::build(ambit::CoreTensor, "F closed", {nactv, nactv});
     auto actv_relative_mos = mo_space_info_->relative_mo("ACTIVE");
-    Fc.iterate([&](const std::vector<size_t>& i, double& value){
+    Fc.iterate([&](const std::vector<size_t>& i, double& value) {
         size_t h1, h2, p, q;
         std::tie(h1, p) = actv_relative_mos[i[0]];
         std::tie(h2, q) = actv_relative_mos[i[1]];
--- forte/mrdsrg-spin-adapted/sa_mrdsrg_diis.cc
+++ forte/mrdsrg-spin-adapted/sa_mrdsrg_diis.cc
@@ -5,7 +5,7 @@
  * that implements a variety of quantum chemistry methods for strongly
  * correlated electrons.
  *
- * Copyright (c) 2012-2022 by its authors (see COPYING, COPYING.LESSER, AUTHORS).
+ * Copyright (c) 2012-2023 by its authors (see COPYING, COPYING.LESSER, AUTHORS).
  *
  * The copyrights for code used from other parties are included in
  * the corresponding files.
@@ -44,13 +44,9 @@ void SA_MRDSRG::diis_manager_init() {
     diis_manager_->set_vector_size(T1_, T2_);
 }
 
-void SA_MRDSRG::diis_manager_add_entry() {
-    diis_manager_->add_entry(DT1_, DT2_, T1_, T2_);
-}
+void SA_MRDSRG::diis_manager_add_entry() { diis_manager_->add_entry(DT1_, DT2_, T1_, T2_); }
 
-void SA_MRDSRG::diis_manager_extrapolate() {
-    diis_manager_->extrapolate(T1_, T2_);
-}
+void SA_MRDSRG::diis_manager_extrapolate() { diis_manager_->extrapolate(T1_, T2_); }
 
 void SA_MRDSRG::diis_manager_cleanup() {
     diis_manager_->reset_subspace();
