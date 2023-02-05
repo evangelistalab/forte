85447bb6c5db290908d912905e2e3a59e93fabad
lcyyork <bjyork0822@gmail.com>
2023-02-06 00:48:59 +0800

    remove json.hpp

--- forte/helpers/disk_io.cc
+++ forte/helpers/disk_io.cc
@@ -40,7 +40,6 @@
 #include "psi4/libpsio/psio.hpp"
 #include "psi4/libpsio/psio.h"
 
-#include "helpers/json.hpp"
 #include "helpers/disk_io.h"
 
 using json = nlohmann::json;
@@ -96,14 +95,32 @@ void read_disk_vector_double(const std::string& filename, std::vector<double>& d
 
 void dump_occupations(const std::string& filename,
                       std::unordered_map<std::string, psi::Dimension> occ_map) {
-    json occ_json;
+    int nirrep = -1;
+    std::vector<std::string> spaces;
     for (const auto& [space_name, dim] : occ_map) {
-        occ_json[space_name] = dim.blocks();
+        if (nirrep == -1) {
+            nirrep = dim.n();
+        } else {
+            if (dim.n() != nirrep)
+                throw std::runtime_error("Inconsistent number of irreps!");
+        }
+        spaces.push_back(space_name);
     }
     std::ofstream wfile(filename + ".json");
     if (wfile.is_open()) {
-        wfile << occ_json.dump(4);
-        wfile.close();
+        wfile << "{";
+        for (int i = 0, n = spaces.size(); i < n; ++i) {
+            wfile << "\n    \"" << spaces[i] << "\": [";
+            for (int h = 0; h < nirrep; ++h) {
+                wfile << occ_map.at(spaces[i])[h];
+                if (h < (nirrep - 1))
+                    wfile << ",";
+            }
+            wfile << "]";
+            if (i < (n - 1))
+                wfile << ",";
+        }
+        wfile << "\n}";
     } else {
         throw std::runtime_error("Unable to open file for dump occupation numbers!");
     }
