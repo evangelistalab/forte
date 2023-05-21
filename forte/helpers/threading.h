7d53758c138357994ec21ad2c4b48212f82627fc
fevangelista <francesco.evangelista@emory.edu>
2023-05-20 23:33:17 -0400

    Add files

---
+++ forte/helpers/threading.h
@@ -0,0 +1,43 @@
+/*
+ * @BEGIN LICENSE
+ *
+ * Forte: an open-source plugin to Psi4 (https://github.com/psi4/psi4)
+ * that implements a variety of quantum chemistry methods for strongly
+ * correlated electrons.
+ *
+ * Copyright (c) 2012-2023 by its authors (see COPYING, COPYING.LESSER, AUTHORS).
+ *
+ * The copyrights for code used from other parties are included in
+ * the corresponding files.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * @END LICENSE
+ */
+
+#ifndef _threading_h_
+#define _threading_h_
+
+namespace forte {
+
+/// @brief Compute the start and end indices for a workload distributed among multiple threads
+/// @param n the total number of elements
+/// @param num_thread the number of threads
+/// @param tid the thread id
+/// @return a pair of start and end indices
+std::pair<size_t, size_t> thread_range(size_t n, size_t num_thread, size_t tid);
+
+} // namespace forte
+
+#endif
