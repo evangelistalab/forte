ed27928e17d0c068d89aed9e9023115daa06c349
fevangelista <francesco.evangelista@emory.edu>
2023-12-03 20:13:25 -0500

    Fix pytests

--- tests/pytest/sparse_ci/srcc/test_uccsd_5.py
+++ tests/pytest/sparse_ci/srcc/test_uccsd_5.py
@@ -14,19 +14,17 @@ def test_uccsd_3():
 
     ref_energy = -107.655681875111
 
-    psi4.set_options({'FORTE__FROZEN_DOCC': [2]})
-    options = forte.prepare_forte_options()
-    forte_objects = forte.prepare_forte_objects_from_fcidump(options, os.path.dirname(__file__))
-    state_weights_map, mo_space_info, scf_info, fcidump = forte_objects
-    ints = forte.make_ints_from_fcidump(fcidump, options, mo_space_info)
-    as_ints = forte.make_active_space_ints(mo_space_info, ints, 'CORRELATED', [])
-    calc_data = scc.run_cc(as_ints, scf_info, mo_space_info, cc_type='ucc', max_exc=2, e_convergence=1.0e-10)
+    psi4.set_options({"FORTE__FROZEN_DOCC": [2]})
+    data = forte.modules.OptionsFactory().run()
+    data = forte.modules.ObjectsFactoryFCIDUMP().run(data)
+    as_ints = forte.make_active_space_ints(data.mo_space_info, data.ints, "CORRELATED", [])
+    calc_data = scc.run_cc(as_ints, data.scf_info, data.mo_space_info, cc_type="ucc", max_exc=2, e_convergence=1.0e-10)
 
     psi4.core.clean()
 
     energy = calc_data[-1][1]
 
-    print(f'  UCCSD energy: {energy}')
+    print(f"  UCCSD energy: {energy}")
     assert energy == pytest.approx(ref_energy, 1.0e-9)
 
 
