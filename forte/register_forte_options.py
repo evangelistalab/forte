a003fe23c51bb8a5480957974cf3ea822d1438d3
marink2 <kmarin3@emory.edu>
2024-01-22 10:17:17 -0500

    fixed yaml file

--- forte/register_forte_options.py
+++ forte/register_forte_options.py
@@ -1,7 +1,13 @@
+import os
+
 import yaml
 
 
-def parse_yaml_file(file_path="/Users/laptop/src/forte/forte/register_forte_options.yaml"):
+def parse_yaml_file(file_name="register_forte_options.yaml"):
+    script_path = os.path.abspath(__file__)
+    script_directory = os.path.dirname(script_path)
+    file_path = os.path.join(script_directory, file_name)
+
     with open(file_path, "r") as file:
         try:
             yaml_dic = yaml.safe_load(file)
@@ -13,7 +19,6 @@ def parse_yaml_file(file_path="/Users/laptop/src/forte/forte/register_forte_opti
 
 def register_forte_options(options):
     yaml_dic = parse_yaml_file()
-
     for group in yaml_dic:
         options.set_group(group)
 
@@ -25,11 +30,17 @@ def register_forte_options(options):
             if opt_typ == "bool":
                 options.add_bool(key, opt_val, opt_msg)
             if opt_typ == "double":
-                options.add_double(key, opt_val, opt_msg)
+                if opt_val == "None":
+                    options.add_double(key, None, opt_msg)
+                else:
+                    options.add_double(key, opt_val, opt_msg)
             if opt_typ == "double_list":
                 options.add_double_list(key, opt_msg)
             if opt_typ == "int":
-                options.add_int(key, opt_val, opt_msg)
+                if opt_val == "None":
+                    options.add_int(key, None, opt_msg)
+                else:
+                    options.add_int(key, opt_val, opt_msg)
             if opt_typ == "int_list":
                 options.add_int_list(key, opt_msg)
             if opt_typ == "list":
