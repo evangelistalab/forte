dd445c73e92545d43af8a0c198075b00c1142bd0
Shuhang Li <shuhangli98@gmail.com>
2024-03-22 09:33:35 -0400

    Reset test.

--- tests/pytest/sparse_ci/test_sparse_ci.py
+++ tests/pytest/sparse_ci/test_sparse_ci.py
@@ -17,32 +17,26 @@ def test_sparse_ci():
     # need to clean the options otherwise this job will interfere
     forte.clean_options()
 
-    psi4.geometry("""
+    psi4.geometry(
+        """
      H
      H 1 1.0
-    """)
+    """
+    )
 
-    psi4.set_options({'basis': 'sto-3g'})
-    E_scf, wfn = psi4.energy('scf', return_wfn=True)
+    psi4.set_options({"basis": "sto-3g"})
+    E_scf, wfn = psi4.energy("scf", return_wfn=True)
     na = wfn.nalpha()
     nb = wfn.nbeta()
     nirrep = wfn.nirrep()
     wfn_symmetry = 0
 
-    psi4_options = psi4.core.get_options()
-    psi4_options.set_current_module('FORTE')
-    forte_options.get_options_from_psi4(psi4_options)
+    # Make the integrals
+    data = forte.modules.ObjectsUtilPsi4().run()
+    as_ints = data.as_ints
 
-    # Setup forte and prepare the active space integral class
-    nmopi = wfn.nmopi()
-    point_group = wfn.molecule().point_group().symbol()
-    mo_space_info = forte.make_mo_space_info(nmopi, point_group, forte_options)
-    ints = forte.make_ints_from_psi4(wfn, forte_options, mo_space_info)
-    as_ints = forte.make_active_space_ints(mo_space_info, ints, 'ACTIVE', ['RESTRICTED_DOCC'])
-    as_ints.print()
-
-    print('\n\n  => Sparse FCI Test <=')
-    print('  Number of irreps: {}'.format(nirrep))
+    print("\n\n  => Sparse FCI Test <=")
+    print("  Number of irreps: {}".format(nirrep))
     nmo = wfn.nmo()
     nmopi = [wfn.nmopi()[h] for h in range(nirrep)]
     nmopi_str = [str(wfn.nmopi()[h]) for h in range(nirrep)]
@@ -51,18 +45,18 @@ def test_sparse_ci():
         for i in range(nmopi[h]):
             mo_sym.append(h)
 
-    print('  Number of orbitals per irreps: [{}]'.format(','.join(nmopi_str)))
-    print('  Symmetry of the MOs: ', mo_sym)
+    print("  Number of orbitals per irreps: [{}]".format(",".join(nmopi_str)))
+    print("  Symmetry of the MOs: ", mo_sym)
 
     hf_reference = forte.Determinant()
     hf_reference.create_alfa_bit(0)
     hf_reference.create_beta_bit(0)
-    print('  Hartree-Fock determinant: {}'.format(hf_reference.str(2)))
+    print("  Hartree-Fock determinant: {}".format(hf_reference.str(2)))
 
     # Compute the HF energy
     hf_energy = as_ints.nuclear_repulsion_energy() + as_ints.slater_rules(hf_reference, hf_reference)
-    print('  Nuclear repulsion energy: {}'.format(as_ints.nuclear_repulsion_energy()))
-    print('  Reference energy: {}'.format(hf_energy))
+    print("  Nuclear repulsion energy: {}".format(as_ints.nuclear_repulsion_energy()))
+    print("  Reference energy: {}".format(hf_energy))
 
     # Build a list of determinants
     orblist = [i for i in range(nmo)]
@@ -77,9 +71,9 @@ def test_sparse_ci():
             for b in bstr:
                 d.create_beta_bit(b)
                 sym = sym ^ mo_sym[b]
-            if (sym == wfn_symmetry):
+            if sym == wfn_symmetry:
                 dets.append(d)
-                print('  Determinant {} has symmetry {}'.format(d.str(nmo), sym))
+                print("  Determinant {} has symmetry {}".format(d.str(nmo), sym))
 
     # Build the Hamiltonian matrix using 'slater_rules'
     nfci = len(dets)
@@ -95,7 +89,7 @@ def test_sparse_ci():
     # Find the lowest eigenvalue
     efci = np.linalg.eigh(H)[0][0]
 
-    print('\n  FCI Energy: {}\n'.format(efci))
+    print("\n  FCI Energy: {}\n".format(efci))
 
     assert efci == pytest.approx(ref_fci, 1.0e-9)
 
