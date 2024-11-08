af77f4e0c0cef308c979857ccb0f941c1949c5de
Harper Grimsley <hrgrimsl@Harpers-MacBook-Air-2.local>
2024-11-08 09:41:46 -0500

    testing test

--- tests/pytest/srcc/test_uccsd_5.py
+++ tests/pytest/srcc/test_uccsd_5.py
@@ -4,7 +4,7 @@
 import pytest
 
 
-def test_uccsd_3():
+def test_uccsd_5():
     """Test projective UCCSD on Ne using RHF/cc-pVDZ orbitals"""
 
     import forte.proc.scc as scc
@@ -18,17 +18,16 @@ def test_uccsd_3():
     data = forte.modules.OptionsFactory().run()
     data = forte.modules.ObjectsFromFCIDUMP(file=os.path.dirname(__file__) + "/INTDUMP").run(data)
     data = forte.modules.ActiveSpaceInts("CORRELATED", []).run(data)
-    calc_data = scc.run_cc(
-        data.as_ints, data.scf_info, data.mo_space_info, cc_type="ucc", max_exc=2, e_convergence=1.0e-10
-    )
+    cc = forte.modules.GeneralCC(cc_type = "ucc", max_exc=2, e_convergence=1.0e-10)
+    data = cc.run(data)
 
     psi4.core.clean()
 
-    energy = calc_data[-1][1]
+    energy = data.results.value("energy")
 
     print(f"  UCCSD energy: {energy}")
     assert energy == pytest.approx(ref_energy, 1.0e-9)
 
 
 if __name__ == "__main__":
-    test_uccsd_3()
+    test_uccsd_5()
