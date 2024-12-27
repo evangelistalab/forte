63fad37d06196288b3f787c61d4a1bdd33512c4b
fevangelista <francesco.evangelista@emory.edu>
2024-12-26 21:18:41 -0500

    Remove fmt

--- forte/sparse_ci/sparse_hamiltonian.cc
+++ forte/sparse_ci/sparse_hamiltonian.cc
@@ -349,8 +349,8 @@ SparseOperator SparseHamiltonian::to_sparse_operator() const {
                                   as_ints_->frozen_core_energy());
     for (size_t p = 0; p < nmo; p++) {
         for (size_t q = 0; q < nmo; q++) {
-            H.add_term_from_str(fmt::format("{}a+ {}a-", p, q), as_ints_->oei_a(p, q));
-            H.add_term_from_str(fmt::format("{}b+ {}b-", p, q), as_ints_->oei_b(p, q));
+            H.add_term_from_str(std::format("{}a+ {}a-", p, q), as_ints_->oei_a(p, q));
+            H.add_term_from_str(std::format("{}b+ {}b-", p, q), as_ints_->oei_b(p, q));
         }
     }
 
@@ -358,9 +358,9 @@ SparseOperator SparseHamiltonian::to_sparse_operator() const {
         for (size_t q = p + 1; q < nmo; q++) {
             for (size_t r = 0; r < nmo; r++) {
                 for (size_t s = r + 1; s < nmo; s++) {
-                    H.add_term_from_str(fmt::format("{}a+ {}a+ {}a- {}a-", p, q, s, r),
+                    H.add_term_from_str(std::format("{}a+ {}a+ {}a- {}a-", p, q, s, r),
                                         as_ints_->tei_aa(p, q, r, s));
-                    H.add_term_from_str(fmt::format("{}b+ {}b+ {}b- {}b-", p, q, s, r),
+                    H.add_term_from_str(std::format("{}b+ {}b+ {}b- {}b-", p, q, s, r),
                                         as_ints_->tei_bb(p, q, r, s));
                 }
             }
@@ -371,7 +371,7 @@ SparseOperator SparseHamiltonian::to_sparse_operator() const {
         for (size_t q = 0; q < nmo; q++) {
             for (size_t r = 0; r < nmo; r++) {
                 for (size_t s = 0; s < nmo; s++) {
-                    H.add_term_from_str(fmt::format("{}a+ {}b+ {}b- {}a-", p, q, s, r),
+                    H.add_term_from_str(std::format("{}a+ {}b+ {}b- {}a-", p, q, s, r),
                                         as_ints_->tei_ab(p, q, r, s));
                 }
             }
